5bca6e2b498d625f519db45f969b23be9f99e6f2
nikhildobariya98 <dobariyanikhil98@gmail.com>
2022-08-18 15:04:30 +0530

    Version 1

---
+++ Employees_3.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+
+class Employee{
+  public:
+  	int id, age, exp;
+	  string name, role, salary, city, company_name;
+	  
+  public:
+	void setEmp(){
+		cout << "Enter ID: ";
+		cin >> id;
+		cout << "Enter Name: ";
+		cin >> name;
+		cout << "Enter Age: ";
+		cin >> age;
+		cout << "Enter Company Name: ";
+		cin >> company_name;
+		cout << "Enter Role: ";
+		cin >> role;
+		cout << "Enter Salary: ";
+		cin >> salary;
+		cout << "Enter Experince: ";
+		cin >> exp;
+		cout << "Enter City: ";
+		cin >> city;
+	} 
+	
+	void getEmp(){
+		cout << id << " " << name << " " << company_name << " " << role << " "
+		<< salary << " " << exp << " " << city;
+	}
+};
+
+int main(){
+	Employee emp[5];
+	
+	for(int i = 0; i < 2; i++){
+		cout << "Enter the detail of employee " << i+1 << endl;
+		emp[i].setEmp();
+	}
+	
+	cout << endl;
+	for(int i = 0; i < 2; i++){
+		cout << "Detail of employee " << i+1 << endl;
+		emp[i].getEmp();
+		cout << endl;
+	}
+	
+	for(int i = 0; i < 2; i++){
+		cout << "Detail of employee " << i+1 << endl;
+		emp[i].getEmp();
+		cout << endl;
+		
+	}
+	for(int i = 0; i < 2; i++){
+		cout << "Detail of employee " << i+1 << endl;
+		emp[i].getEmp();
+		cout << endl;
+		
+	}
+	
+	for(int i = 0; i < 2; i++){
+		cout << "Detail of employee " << i+1 << endl;
+		emp[i].getEmp();
+		cout << endl;
+	}
+}
